8274270164c04be7790a40b151cf9942397f9f99
Dmitry Korchagin <dmitry.korchagin@axibase.com>
2015-09-01 12:08:08 +0000

    add test connection example
    git-svn-id: https://localhost/svn/axibase/AxibaseAPI/atsd-api-php@10280 c89e5cec-f22d-a84c-b7a1-b0d050e0f212

---
+++ examples/testConnection.php
@@ -0,0 +1,35 @@
+<?php
+/*
+* Copyright 2015 Axibase Corporation or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+* https://www.axibase.com/atsd/axibase-apache-2.0.pdf
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+namespace axibase\atsdPHP;
+require_once '../atsdPHP/models/Entities.php';
+require_once '../atsdPHP/HttpClient.php';
+
+$client = new HttpClient();
+$client->connect();
+
+$expression = 'name like \'*\'';
+$limit = 1;
+
+$queryClient = new Entities($client);
+
+$params = array("limit" => $limit, 'expression' => $expression);
+$responseEntities = $queryClient->findAll($params);
+$client->close();
+if(empty($responseEntities)) {
+    exit("Connection failed!");
+}
+exit("Connection success!");
