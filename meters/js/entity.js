bc479c7c2e45f6b8152d6103115d39567fb5c8e2
Dmitry Korchagin <dmitry.korchagin@axibase.com>
2015-10-09 14:23:47 +0000

    define metric in js
    git-svn-id: https://localhost/svn/axibase/AxibaseAPI/atsd-api-php@10794 c89e5cec-f22d-a84c-b7a1-b0d050e0f212

--- meters/js/entity.js
+++ meters/js/entity.js
@@ -1,7 +1,7 @@
 (function(global, undefined){
-
-global.getReport = function(entity, date, interval) {
-    location.href = "CsvGenerator.php?entity=" + entity + "&endDate=" + date + "&interval=" + interval + "&metric=" + metricValue;
+    var metric = "sml.power-consumed";
+    global.getReport = function(entity, date, interval) {
+    location.href = "CsvGenerator.php?entity=" + entity + "&endDate=" + date + "&interval=" + interval + "&metric=" + metric;
 };
 
 global.generateWidgets = function(entity){
@@ -18,12 +18,15 @@ global.generateWidgets = function(entity){
 
         for(var j = 0; j < daily.series.length; j++) {
             daily.series[j].entity = cEntity;
+            daily.series[j].metric = metric;
         }
         for(var j = 0; j < current.series.length; j++) {
             current.series[j].entity = cEntity;
+            current.series[j].metric = metric;
         }
         for(var j = 0; j < monthly.series.length; j++) {
             monthly.series[j].entity = cEntity;
+            monthly.series[j].metric = metric;
         }
 
         updateWidget(daily, 'daily-usage');
